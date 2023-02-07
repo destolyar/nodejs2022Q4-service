e2b9272aa5ac114cebc02ab24fa5a71a52f017d3
destolyar <dead.stolyar@gmail.com>
2023-02-07 17:53:11 +0300

    Dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config'
 
 async function bootstrap() {
+  const port = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(port);
 }
 
 bootstrap();
